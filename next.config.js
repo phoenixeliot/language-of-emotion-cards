17a917acc92b8526c12bef5b04791dda272fa3da
Phoenix Eliot <phoenixjeliot@gmail.com>
2021-07-18 21:36:13 -0600

    Fix basePath for github pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  basePath: "/language-of-emotion-cards",
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
